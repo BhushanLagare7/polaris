e0669a51a7adfdae6c2568c817b67b32cce59dc6
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-01-08 17:17:17 +0530

    Add Convex backend with authentication and projects

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  // createRouteMatcher,
+} from "@clerk/nextjs/server";
+
+// INFO: USE THIS IS AN ALTERNATIVE WAY TO PROTECT API ROUTES WHEN YOU DON'T WANT TO DISPLAY AN UNAUTHORIZED VIEW OR A SIGN-IN BUTTON FOR UNAUTHORIZED USERS.
+
+// const isPublicRoute = createRouteMatcher(["/api/inngest(.*)"]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     await auth.protect();
+//   }
+// });
 
 export default clerkMiddleware();
 

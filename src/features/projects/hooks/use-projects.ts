78bb1860218907e37ea0364ab262e29a569ae037
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-01-10 21:41:47 +0530

    Update the rename implaementation

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -40,19 +40,19 @@ export const useCreateProject = () => {
   );
 };
 
-export const useRenameProject = (projectId: Id<"projects">) => {
+export const useRenameProject = () => {
   return useMutation(api.projects.rename).withOptimisticUpdate(
     (localStore, args) => {
       const now = Date.now();
 
       const existingProject = localStore.getQuery(api.projects.getById, {
-        id: projectId,
+        id: args.id,
       });
 
       if (existingProject !== undefined && existingProject !== null) {
         localStore.setQuery(
           api.projects.getById,
-          { id: projectId },
+          { id: args.id },
           {
             ...existingProject,
             name: args.name,
@@ -68,7 +68,7 @@ export const useRenameProject = (projectId: Id<"projects">) => {
           api.projects.get,
           {},
           existingProjects.map((project) => {
-            return project._id === projectId
+            return project._id === args.id
               ? {
                   ...project,
                   name: args.name,
